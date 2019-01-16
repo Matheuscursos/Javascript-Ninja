9688ad8b68f20f407141f225e8e88e425b8d5181
Matheus <ms190698@gmail.com>
2019-01-16 11:55:45 -0200

    aula 22 concluida

--- ninja.js
+++ ninja.js
@@ -1,13 +1,7 @@
 (function() {
     'use strict'
 
-    function myFuntion(name,lastName){
-        this.fullName = name + ' ' + lastName;
-        this.lastName = lastName;
-    }
-    myFuntion.prototype.fullName = function(){
-      return this.name + ' ' + this.lastName;
-    }
-    var matheus = new myFuntion('matheus','silva');
-    console.log(fernando.fullName);
+    array. .prototype.methodName = function () {
+
+    };
 })();
