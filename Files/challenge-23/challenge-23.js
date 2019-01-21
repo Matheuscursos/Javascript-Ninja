3bede1c1b03d0b2c9ed98f833b60cca8853d5fd3
Matheus <ms190698@gmail.com>
2019-01-21 11:35:23 -0200

    calculadora concluida

--- Files/challenge-23/challenge-23.js
+++ Files/challenge-23/challenge-23.js
@@ -1,25 +1,97 @@
-/*
-Vamos desenvolver mais um projeto. A ideia é fazer uma mini-calculadora.
-As regras são:
+(function(win, doc) {
+	'use strict';
 
-- Deve ter somente 1 input, mas não deve ser possível entrar dados nesse input
-diretamente;
-- O input deve iniciar com valor zero;
-- Deve haver 10 botões para os números de 0 a 9. Cada botão deve ser um número;
-- Deve haver 4 botões para as operações principais: soma (+), subtração(-),
-multiplicação(x) e divisão(÷);
-- Deve haver um botão de "igual" (=) que irá calcular os valores e um botão "CE"
-que irá limpar o input, deixando-o com valor 0;
+	/*
+	Vamos desenvolver mais um projeto. A ideia é fazer uma mini-calculadora.
+	As regras são:
+	- Deve ter somente 1 input, mas não deve ser possível entrar dados nesse input
+	diretamente;
+	- O input deve iniciar com valor zero;
+	- Deve haver 10 botões para os números de 0 a 9. Cada botão deve ser um número;
+	- Deve haver 4 botões para as operações principais: soma (+), subtração(-),
+	multiplicação(x) e divisão(÷);
+	- Deve haver um botão de "igual" (=) que irá calcular os valores e um botão "CE"
+	que irá limpar o input, deixando-o com valor 0;
+	- A cada número pressionado, o input deve atualizar concatenando cada valor
+	digitado, como em uma calculadora real;
+	- Ao pressionar um botão com uma das 4 operações, deve aparecer o símbolo da
+	operação no input. Se o último caractere no input já for um símbolo de alguma
+	operação, esse caractere deve ser substituído pelo último pressionado.
+	Exemplo:
+	- Se o input tem os valores: "1+2+", e for pressionado o botão de
+	multiplicação (x), então no input deve aparecer "1+2x".
+	- Ao pressionar o botão de igual, o resultado do cálculo deve ser mostrado no
+	input;
+	- Ao pressionar o botão "CE", o input deve ficar zerado.
+	*/
 
-- A cada número pressionado, o input deve atualizar concatenando cada valor
-digitado, como em uma calculadora real;
-- Ao pressionar um botão com uma das 4 operações, deve aparecer o símbolo da
-operação no input. Se o último caractere no input já for um símbolo de alguma
-operação, esse caractere deve ser substituído pelo último pressionado.
-Exemplo:
-- Se o input tem os valores: "1+2+", e for pressionado o botão de
-multiplicação (x), então no input deve aparecer "1+2x".
-- Ao pressionar o botão de igual, o resultado do cálculo deve ser mostrado no
-input;
-- Ao pressionar o botão "CE", o input deve ficar zerado.
-*/
+
+	var $tela = doc.querySelector('[data-js="tela"]');
+	var $numbers = doc.querySelectorAll('[data-js="button-number"]');
+	var $ce = doc.querySelector('[data-js="button-ce"]');
+	var $operation = doc.querySelectorAll('[data-js="button-operation"]');
+	var $equal = doc.querySelector('[data-js="button-equal"]')
+
+	Array.prototype.forEach.call($numbers, function(button) {
+		button.addEventListener('click', ClickNumber, false);
+	});
+	Array.prototype.forEach.call($operation, function(button) {
+		button.addEventListener('click', ClickOperation, false);
+	});
+
+	$ce.addEventListener('click', limpaTela, false);
+	$equal.addEventListener('click', resultado, false);
+
+	function ClickOperation() {
+		$tela.value = removeLastItem($tela.value);
+		$tela.value += this.value;
+	}
+
+	function isLastItemAnOpretation(number) {
+		var operation = ['+', '-', '*', '÷'];
+		var lastItem = number.split('').pop();
+		return operation.some(function(operator) {
+			return operator === lastItem;
+		});
+	}
+
+	function removeLastItem(number) {
+		if (isLastItemAnOpretation(number)) {
+			return number.slice(0, -1);
+		}
+		return number;
+	}
+
+	function ClickNumber() {
+
+		$tela.value += this.value;
+	}
+
+	function limpaTela() {
+		$tela.value = 0;
+	}
+
+	function resultado() {
+		$tela.value = removeLastItem($tela.value);
+		var allValues = $tela.value.match(/\d+[+*÷-]?/g);
+		$tela.value = allValues.reduce(function(acumulado, atual) {
+			var firstValue = acumulado.slice(0, -1);
+			var operador = acumulado.split('').pop();
+			var lastValue = removeLastItem(atual);
+			var lastOperator = isLastItemAnOpretation(atual) ? atual.split('').pop(): '';
+			switch (operador) {
+				case '+':
+					return ( Number(firstValue) +  Number(lastValue)  ) + lastOperator;
+				case '-':
+					return  ( Number(firstValue) -  Number(lastValue) ) + lastOperator;
+				case '*':
+					return  ( Number(firstValue) *  Number(lastValue) ) + lastOperator;
+				case '÷':
+					return  ( Number(firstValue) /  Number(lastValue) ) + lastOperator;
+			}
+		});
+
+
+	}
+
+})(window, document);
